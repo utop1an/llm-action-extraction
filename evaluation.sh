1296c96f14815fe14cfbea100a8eb80f57538cf2
utop1an <vfqq1066@outlook.com>
2026-01-11 00:34:30 +1100

    eval bash

--- evaluation.sh
+++ evaluation.sh
@@ -1,12 +1,11 @@
 PREFIX="./results"
+SOLVER=nl2p_1
 
-for solver in nl2p_1, gpt3_to_plan; do
-    for model in gemma3, gemma3_12b, llama3.2; do
-        echo "Evaluating solver: $solver with model: $model"
+for model in gemma3 gemma3_12b llama3.2 gpt-4.1 gpt-4.1-mini gpt-4.1-nano gpt-4o gpt-4o-mini; do
+    echo "Evaluating solver: $solver with model: $model"
 
-        python3 evaluation.py \
-            -d "$PREFIX/${solver}/${model}/"
+    python3 evaluation.py \
+        -d "$PREFIX/${solver}/${model}/"
 
-        echo "Completed evaluation for solver: $solver with model: $model"
-    done
+    echo "Completed evaluation for solver: $solver with model: $model"
 done
