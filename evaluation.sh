650cdbc14e41802139d2ced8dca27b0c506c1aa1
Siqi <siqi.cheng@anu.edu.au>
2026-01-19 19:40:19 +1100

    add missing models to evaluation.sh

--- evaluation.sh
+++ evaluation.sh
@@ -1,6 +1,6 @@
 PREFIX="./results/experiment"
 SOLVERS="gpt3_to_plan nl2p_1 nl2p_2 nl2p_3"
-MODELS="gemma3 gemma3_12b llama3.2 gpt-4.1 gpt-4.1-mini gpt-4.1-nano"
+MODELS="gemma3 gemma3_12b llama3.2 gpt-4.1 gpt-4.1-mini gpt-4.1-nano gpt-4o gpt-4o-mini"
 
 for solver in $SOLVERS; do
     for model in $MODELS; do
